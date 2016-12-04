ad5d2cd7ae75e4563957181e9bbbce9300f9ec2a
yuan12345 <2045509291@qq.com>
2016-12-04 13:08:25 +0800

    Toobar ,TabLayout

---
+++ app/src/main/java/com/adapter/MyPagerAdapter.java
@@ -0,0 +1,48 @@
+package com.adapter;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Administrator on 2016-12-4.
+ */
+
+public class MyPagerAdapter extends PagerAdapter {
+
+    ArrayList<TextView> textViews;
+
+    public MyPagerAdapter(ArrayList<TextView> textViews) {
+        this.textViews = textViews;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return textViews.get(position).getText();
+    }
+
+    @Override
+    public int getCount() {
+        return textViews.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        container.addView(textViews.get(position));
+        return textViews.get(position);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView(((View) object));
+    }
+}
--- app/src/main/java/com/everyoo/materialdesign/MainActivity.java
+++ app/src/main/java/com/everyoo/materialdesign/MainActivity.java
@@ -1,11 +1,13 @@
 package com.everyoo.materialdesign;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
@@ -111,6 +113,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable s) {
 
+            }
+        });
+
+
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ToolbarActivity.class));
             }
         });
 
