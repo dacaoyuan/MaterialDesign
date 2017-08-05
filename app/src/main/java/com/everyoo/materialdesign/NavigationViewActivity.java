ee254de5f8782b29c4e092608b82248abba052ae
ypk <2908363622@qq.com>
2017-08-05 15:17:19 +0800

    完善了md 的侧边菜功能

--- app/src/main/java/com/everyoo/materialdesign/NavigationViewActivity.java
+++ app/src/main/java/com/everyoo/materialdesign/NavigationViewActivity.java
@@ -1,11 +1,14 @@
 package com.everyoo.materialdesign;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -13,29 +16,70 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class NavigationViewActivity extends AppCompatActivity {
     private static final String TAG = "NavigationViewActivity";
     private DrawerLayout mDrawerLayout;
     private NavigationView mNavigationView;
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //透明状态栏效果
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+        }
+
+
         setContentView(R.layout.activity_navigation_view);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.id_drawer_layout);
         mNavigationView = (NavigationView) findViewById(R.id.id_nv_menu);
+        view = mNavigationView.inflateHeaderView(R.layout.header_just_username);
+        TextView textViewName = (TextView) view.findViewById(R.id.id_username);
+        textViewName.setText("袁培凯");
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.id_toolbar);
+
+        Button openMenuBtn = (Button) findViewById(R.id.openMenu);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final ActionBar ab = getSupportActionBar();
+       /* final ActionBar ab = getSupportActionBar();
         ab.setHomeAsUpIndicator(R.mipmap.bg_personal_center_number);
-        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setDisplayHomeAsUpEnabled(true);*/
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
         setupDrawerContent(mNavigationView);
+
+
+        openMenuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    mDrawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    mDrawerLayout.openDrawer(GravityCompat.START);
+                }
+            }
+        });
+
+        mDrawerLayout.openDrawer(GravityCompat.START);
 
 
     }
@@ -45,10 +89,6 @@ public class NavigationViewActivity extends AppCompatActivity {
         mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                System.out.println("NavigationViewActivity.onNavigationItemSelected");
-                Log.i(TAG, "onNavigationItemSelected:  item.getItemId()= " + item.getItemId());
-                Log.i(TAG, "onNavigationItemSelected:  item.toString()= " + item.toString());
-
                 Log.i(TAG, "onNavigationItemSelected:  item.getTitle()= " + item.getTitle());
                 Log.i(TAG, "onNavigationItemSelected:  item.getSubMenu()= " + item.getSubMenu());
                 Log.i(TAG, "onNavigationItemSelected:  item.getGroupId()= " + item.getGroupId());
@@ -56,40 +96,59 @@ public class NavigationViewActivity extends AppCompatActivity {
                 Log.i(TAG, "onNavigationItemSelected:  item.getIcon()= " + item.getIcon());
                 Log.i(TAG, "onNavigationItemSelected:  item.getTitleCondensed()= " + item.getTitleCondensed());
 
-                if (!TextUtils.isEmpty(item.getTitle()) && item.getTitle().equals("ToolBar")) {
-                    startActivity(new Intent(NavigationViewActivity.this, ToolbarActivity.class));
-                    item.setChecked(true);
-                    //item.setIcon(R.mipmap.bg_personal_center_number);
-                }
-                if (!TextUtils.isEmpty(item.getTitle()) && item.getTitle().equals("TabLayout")) {
-                    startActivity(new Intent(NavigationViewActivity.this, TabLayoutActivity.class));
-                    item.setChecked(true);
-                    //item.setIcon(R.mipmap.bg_personal_center_number);
-                }
-                if (!TextUtils.isEmpty(item.getTitle()) && item.getTitle().equals("FloatingActionButton")) {
-                    startActivity(new Intent(NavigationViewActivity.this, FloatingActionButtonActivity.class));
-                    item.setChecked(true);
-                    //item.setIcon(R.mipmap.bg_personal_center_number);
-                }
+                Intent intent = null;
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        intent = new Intent(NavigationViewActivity.this, ToolbarActivity.class);
+                        //item.setIcon(R.mipmap.bg_personal_center_number);
+                        break;
+                    case R.id.nav_tabLayout:
+                        intent = new Intent(NavigationViewActivity.this, TabLayoutActivity.class);
+                        //item.setIcon(R.mipmap.bg_personal_center_number);
 
-                if (!TextUtils.isEmpty(item.getTitle()) && item.getTitle().equals("BottomNavigationView")) {
-                    startActivity(new Intent(NavigationViewActivity.this, BottomNavigationViewActivity.class));
-                    item.setChecked(true);
-                    //item.setIcon(R.mipmap.bg_personal_center_number);
+                        break;
+                    case R.id.nav_floatingActionButton:
+                        intent = new Intent(NavigationViewActivity.this, TabLayoutActivity.class);
+                        //item.setIcon(R.mipmap.bg_personal_center_number);
+
+                        break;
+                    case R.id.nav_BottomNavigationView:
+                        intent = new Intent(NavigationViewActivity.this, BottomNavigationViewActivity.class);
+                        //item.setIcon(R.mipmap.bg_personal_center_number);
+
+                        break;
+                    case R.id.nav_animation:
+                        intent = new Intent(NavigationViewActivity.this, AnimationActivity.class);
+                        //item.setIcon(R.mipmap.bg_personal_center_number);
+
+                        break;
+                    case R.id.sub01:
+                        Log.i(TAG, "onNavigationItemSelected: sub01");
+                        intent = new Intent(NavigationViewActivity.this, AnimationActivity.class);
+                        break;
+                    case R.id.sub02:
+                        Log.i(TAG, "onNavigationItemSelected: sub02");
+                        intent = new Intent(NavigationViewActivity.this, AnimationActivity.class);
+
+                        break;
+
                 }
-
-                if (!TextUtils.isEmpty(item.getTitle()) && item.getTitle().equals("Animation")) {
-                    startActivity(new Intent(NavigationViewActivity.this, AnimationActivity.class));
-                    item.setChecked(true);
-                    //item.setIcon(R.mipmap.bg_personal_center_number);
-                }
-
-
+                startActivity(intent);
                 item.setChecked(true);
-                // mDrawerLayout.closeDrawers();//关闭所有当前打开抽屉视图
+                mDrawerLayout.closeDrawers();//关闭所有当前打开抽屉视图
                 return true;
             }
         });
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     /**
@@ -100,9 +159,25 @@ public class NavigationViewActivity extends AppCompatActivity {
      */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_drawer, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+
+
+      /*  这两个方法配合使用
+      final ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.mipmap.bg_personal_center_number);
+        ab.setDisplayHomeAsUpEnabled(true);*/
+
+   /* @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            mDrawerLayout.openDrawer(GravityCompat.START);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
 
 
     /**
@@ -113,10 +188,15 @@ public class NavigationViewActivity extends AppCompatActivity {
      */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            mDrawerLayout.openDrawer(GravityCompat.START);
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/everyoo/materialdesign/ToolbarActivity.java
+++ app/src/main/java/com/everyoo/materialdesign/ToolbarActivity.java
@@ -1,9 +1,15 @@
 package com.everyoo.materialdesign;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -12,15 +18,27 @@ public class ToolbarActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_toolbar);
+        //透明状态栏效果
+        /*if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+        }*/
+
+        //setContentView(R.layout.activity_toolbar);
+        setContentView(R.layout.activity_toolbar1);
 
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("这里是Title");
-        toolbar.setSubtitle("这里是子标题");
-        toolbar.setLogo(R.mipmap.bg_personal_center_number);
+        toolbar.setTitle("");
+        //toolbar.setSubtitle("这里是子标题");
+        //toolbar.setLogo(R.mipmap.bg_personal_center_number);
         setSupportActionBar(toolbar);
-
+        final ActionBar ab = getSupportActionBar();
+        //ab.setHomeAsUpIndicator(R.mipmap.bg_personal_center_number);
+        ab.setDisplayHomeAsUpEnabled(true);
 
         toolbar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,5 +48,34 @@ public class ToolbarActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+
+    /**
+     * 在创建选项菜单
+     *
+     * @param menu
+     * @return
+     */
+  /*  @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }*/
+
+
+    /**
+     * 在选择项选择
+     *
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
